8e11fa0496e2bbde2de9bb6c82ff78df67637674
Marcus Durao <mvdurao@gmail.com>
2023-06-27 21:50:33 -0300

    Criando entidade categoria

---
+++ domain/src/main/java/com/fullcycle/catalogo/video/domain/Identifier.java
@@ -0,0 +1,4 @@
+package com.fullcycle.catalogo.video.domain;
+
+public abstract class Identifier extends ValueObject {
+}
---
+++ domain/src/main/java/com/fullcycle/catalogo/video/domain/pagination/Pagination.java
@@ -0,0 +1,11 @@
+package com.fullcycle.catalogo.video.domain.pagination;
+
+import java.util.List;
+
+public record Pagination<T>(
+        int currentPage,
+        int perPage,
+        long total,
+        List<T> items
+) {
+}
